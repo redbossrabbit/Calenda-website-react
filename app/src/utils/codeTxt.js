442202d5f13d5a2067bdc4d69683dd76863f7f2b
= <=>
2021-01-26 12:47:40 +0100

    commit

--- app/src/utils/codeTxt.js
+++ app/src/utils/codeTxt.js
@@ -1,19 +1,16 @@
 const languageData = [
-    [
-        'npm install --save-dev calenda', 'terminal'
-    ],
-    [
-        `Import Calenda from 'calenda'
+  ["npm i calenda", "terminal"],
+  [
+    `Import Calenda from 'calenda'
 
 const calendar = new Calenda({
   month: 'January'
-});`, 'javascript'
-    ],
-    [
-        `calendar.getStructure().structure;`, 'javascript'
-    ],
-    [
-        `{
+});`,
+    "javascript"
+  ],
+  [`calendar.getStructure().structure;`, "javascript"],
+  [
+    `{
   Sun: [ 0, 3, 10, 17, 24, 31 ],
   Mon: [ 0, 4, 11, 18, 25 ],
   Tue: [ 0, 5, 12, 19, 26 ],
@@ -21,43 +18,48 @@ const calendar = new Calenda({
   Thu: [ 0, 7, 14, 21, 28 ],
   Fri: [ 1, 8, 15, 22, 29 ],
   Sat: [ 2, 9, 16, 23, 30 ]
-}`, 'terminal'
-    ],
-    [
-        `calendar.getStructure().getInfo(1, {
+}`,
+    "terminal"
+  ],
+  [
+    `calendar.getStructure().getInfo(1, {
   dayNumber: true,
   dayOfWeek: true,
   monthOfYear: true,
   year: true,
   suffix: true
-})`, 'javascript'
-    ],
-    [
-        `{
+})`,
+    "javascript"
+  ],
+  [
+    `{
   dayNumber: 1,
   dayOfWeek: 'Fri',
   monthOfYear: 'January',
   year: 2021,
   suffix: 'st'
-}`, 'terminal'
-    ],
-    [
-        `calendar.moveMonth('forward').getStructure().getInfo(1, {
+}`,
+    "terminal"
+  ],
+  [
+    `calendar.moveMonth('forward').getStructure().getInfo(1, {
   dayNumber: true,
   dayOfWeek: true,
   monthOfYear: true,
   year: true,
   suffix: true
-})`, 'javascript'
-    ],
-    [
-        `{
+})`,
+    "javascript"
+  ],
+  [
+    `{
   dayNumber: 1,
   dayOfWeek: 'Mon',
   monthOfYear: 'February',
   year: 2021,
   suffix: 'st'
-}`, 'terminal'
-    ]
+}`,
+    "terminal"
+  ]
 ];
 export default languageData;
